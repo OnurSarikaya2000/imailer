fbdf6b9c840112893899f5f53f12c476bd0132c2
Tom Hardman <tomhardman0@gmail.com>
2015-11-11 18:12:34 +0000

    send message test works

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,17 @@
+const messages = require('osa-imessage')
+
 module.exports = function(app) {
 
   app.get('/', function(req, res) {
-    res.render('pages/home', {
-      title: process.env.APP_NAME
+    console.log('sending')
+
+    messages.send('Let me know if you receive this... it\'s a test :) x', process.env.NUMBER1, function() {
+      console.log('sent')
+      res.render('pages/home', {
+        title: process.env.APP_NAME
+      })
     })
+
   })
 
 }
