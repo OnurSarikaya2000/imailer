610156a1e9187fa1c73848e800914eb4de5fb329
Tom Hardman <tomhardman0@gmail.com>
2016-02-23 21:27:34 +0000

    contact names now work

--- index.js
+++ index.js
@@ -3,25 +3,39 @@
 const exec = require('child_process').exec;
 const colors = require('colors');
 
+const sms = 'service "SMS"';
+const imessage = '(service 1 whose service type is iMessage)';
+
 let userArgs = process.argv.slice(2);
 let to = userArgs[0];
 let message = userArgs[1];
-
-let sms = 'service "SMS"';
-let imessage = '(service 1 whose service type is iMessage)';
 let method = userArgs[2] === 'sms' ? sms : imessage;
+let command;
 
-// Create command
-let command = `/usr/bin/osascript -e 'tell application "Messages"
+// If string contains letters, it must be a contact name so
+// find the relevant phone number first
+if (/[a-z]/i.test(to)) {
+    command = `/usr/bin/osascript -e 'tell application "Contacts"
+                set i to value of phone 1 of (person 1 whose name = "${to}")
+                end tell
+                tell application "Messages"
+                send "${message}" to buddy i of ${method}
+                end tell'`;
+} else {
+    command = `/usr/bin/osascript -e 'tell application "Messages"
                 send "${message}" to buddy "${to}" of ${method}
                 end tell'`;
+}
 
 // Check user input
 if (!userArgs[0]) console.error('You didn\'t enter a recipient!'.red);
 else if (!userArgs[1]) console.error('You didn\'t enter a message!'.red);
 else {
     exec(command, (err, stdout, stderr) => {
-        if (err) throw new Error(err);
+        if (err) {
+            if (/-1719/.test(err)) console.error(`We couldn\'t find a number for ${to}`.red);
+            else throw new Error(err);
+        }
         else console.log(`Message sent to ${to}!`.green);
     });
 }
