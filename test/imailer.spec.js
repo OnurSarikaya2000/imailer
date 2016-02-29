23b6e3d436eae10a311cf8084c05b3adc2b19902
Tom Hardman <tomhardman0@gmail.com>
2016-02-29 14:00:05 +0000

    adding tests

---
+++ test/imailer.spec.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const exec = require('child_process').exec;
+
+describe('Test input:', function () {
+    it('should error with no recipient', function (done) {
+        exec('imailer', (err, stdout, stderr) => {
+            assert.equal(err, null);
+            assert.equal(stdout, '');
+            assert.equal(stderr, 'You didn\'t enter a recipient!\n');
+            done();
+        });
+    });
+
+    it('should error with recipient but no message', function (done) {
+        exec('imailer 07533289098', (err, stdout, stderr) => {
+            assert.equal(err, null);
+            assert.equal(stdout, '');
+            assert.equal(stderr, 'You didn\'t enter a message!\n');
+            done();
+        });
+    });
+
+});
