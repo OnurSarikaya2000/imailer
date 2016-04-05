cd5c10f975b190c9bd2a95ee269941069d98e355
Tom Hardman <tomhardman0@users.noreply.github.com>
2016-04-05 11:06:25 +0100

    revert

--- test/imailer.spec.js
+++ test/imailer.spec.js
@@ -3,7 +3,7 @@ const exec = require('child_process').exec;
 
 describe('Test input:', function () {
     it('should error with no recipient', function (done) {
-        exec('/home/travis/build/tomhardman0/imailer', (err, stdout, stderr) => {
+        exec('imailer', (err, stdout, stderr) => {
             assert.equal(err, null);
             assert.equal(stdout, '');
             assert.equal(stderr, 'You didn\'t enter a recipient!\n');
@@ -12,7 +12,7 @@ describe('Test input:', function () {
     });
 
     it('should error with recipient but no message', function (done) {
-        exec('/home/travis/build/tomhardman0/imailer 07533289098', (err, stdout, stderr) => {
+        exec('imailer 07533289098', (err, stdout, stderr) => {
             assert.equal(err, null);
             assert.equal(stdout, '');
             assert.equal(stderr, 'You didn\'t enter a message!\n');
